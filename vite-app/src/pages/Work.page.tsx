0f2f767b461646019e74c4254d81e2d12ade1575
Alan Sandoval <asandov@asandov.com>
2024-07-22 18:04:35 +0000

    add my resume as pdf

--- vite-app/src/pages/Work.page.tsx
+++ vite-app/src/pages/Work.page.tsx
@@ -1,14 +1,48 @@
 import { LinkedinButton } from '@/components/SocialButtons/SocialButtons';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Card, Container, Text } from '@mantine/core';
+import { Card, Center, Container, Space, Text } from '@mantine/core';
+import { useState } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
 
 export function WorkInfoPage() {
+    const [numPages, setNumPages] = useState<number>();
+    const [pageNumber, setPageNumber] = useState<number>(1);
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+        setNumPages(numPages);
+    }
+
     return (
-        <Container size="xs">
-            <Card>
-                <Text>Coming soon... Download my resume and visit me on LinkedIn</Text>
-                <LinkedinButton href="https://www.linkedin.com/in/alan-sandoval" />
+        <Container size="50%">
+            <Card shadow="sm" padding="lg" radius="md" withBorder >
+                {/* <Center> */}
+                <Space h={40} />
+                <Card.Section inheritPadding>
+                    <Text size="lg" ta="center">
+                        I love building things. I am driven to keep learning and to create helpful tools with what I learn. Check out my professional work experience and a few projects in my resume below!
+                    </Text>
+                </Card.Section>
+                {/* </Center> */}
+                <Space h={80} />
+                <Center>
+                    <Card.Section>
+                        <LinkedinButton href="https://www.linkedin.com/in/alan-sandoval" />
+                    </Card.Section>
+                </Center>
+                <Space h={80} />
+                <Center>
+                    <Card.Section>
+                        <Document file="alan-sandoval-resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                            <Page pageNumber={pageNumber} />
+                        </Document>
+                    </Card.Section>
+                </Center>
             </Card>
-        </Container>
+        </Container >
     );
 }
